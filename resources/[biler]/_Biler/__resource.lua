fec8a6decf205d2f1ef428fab02cda3635ec8b4f
Mikkel <44027546+FlaxDoe@users.noreply.github.com>
2022-11-19 06:10:58 -0800

    Add files via upload

---
+++ resources/[biler]/_Biler/__resource.lua
@@ -0,0 +1,25 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+	'data/ems/vehicles.meta',
+    'data/ems/carvariations.meta',
+    'data/ems/carcols.meta',
+    'data/ems/handling.meta',
+	'data/politi/vehicles.meta',
+    'data/politi/carvariations.meta',
+    'data/politi/carcols.meta',
+    'data/politi/handling.meta',
+    'data/job/vehicles.meta',
+    'data/job/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'data/ems/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/ems/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/ems/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/ems/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/politi/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/politi/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/politi/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/politi/carvariations.meta'
+data_file 'CARCOLS_FILE' 'data/job/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/job/handling.meta'
