b8de99f44cd32b229989823fd2d43e63de0268e6
Mikkel <44027546+FlaxDoe@users.noreply.github.com>
2022-11-20 07:16:26 -0800

    Add files via upload

---
+++ resources/[vrp]/vrp-chat/cfg/config.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Apisathan
+-- Date: 11-01-2019
+-- Time: 20:22
+-- Github: https://github.com/Apisathan/FiveM-Scripts
+--
+local cfg = {}
+
+cfg.ckCommand = false
+cfg.pkCommand = false
+cfg.discordWebhook = "https://discordapp.com/api/webhooks/613051517595025418/cKH8sm7a1CElU1OeM_AsXgijwoPm0r3ix24PSb2YV2So2VARXZS-n50U3FtAIPKTTEcP"
+cfg.notifyStaffPerm = "staff.chat"
+
+return cfg
+
+
