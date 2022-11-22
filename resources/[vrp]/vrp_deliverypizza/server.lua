4d2401a321f5336effc1f4f1ce1c43d72c3aea5a
Kian Frostholm <70072668+KianFrostholm@users.noreply.github.com>
2022-11-22 21:20:26 +0100

    Update server.lua

--- resources/[vrp]/vrp_deliverypizza/server.lua
+++ resources/[vrp]/vrp_deliverypizza/server.lua
@@ -1,8 +1,10 @@
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
+local webhook = module("cfg/webhooks")
 
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vrp_deliverypizza")
+
 
 
 RegisterServerEvent('delivery:checkjob1')
@@ -31,7 +33,7 @@ AddEventHandler('delivery:success1', function(amount)
         TriggerClientEvent("pNotify:SendNotification", source,{text = "Du modtog <b style='color: #4E9350'>" .. toPay.." DKK</b>.",type = "success",timeout = (5000),layout = "centerRight",queue = "global",animation = {open = "gta_effects_fade_in", close = "gta_effects_fade_out"}})
     else
         vRP.ban({user_id, 'Du forsøgte og spawne '..amount..' via "vrp_pizzadelivery"'})
-        PerformHttpRequest('DIT_WEBHOOK', function(err, text, headers) end, 'POST', 
+        PerformHttpRequest(webhook.HoneyPot, function(err, text, headers) end, 'POST', 
         json.encode({username = 'Kian - Logs', 
         content = 'ID: '..user_id..' Forsøgte og spawne '..amount..' DKK via '..GetCurrentResourceName()}), {['Content-Type'] = 'application/json'})
     end
