e33026c48a2011cfd67d5010c08975c955624045
Kian Frostholm <70072668+KianFrostholm@users.noreply.github.com>
2022-11-23 20:32:27 +0100

    Update sv_queue_config.lua

--- resources/[system]/connectqueue/server/sv_queue_config.lua
+++ resources/[system]/connectqueue/server/sv_queue_config.lua
@@ -1,57 +1,39 @@
 Config = {}
 
--- priority list can be any identifier. (hex steamid, steamid32, ip) Integer = power over other people with priority
--- a lot of the steamid converting websites are broken rn and give you the wrong steamid. I use https://steamid.xyz/ with no problems.
--- you can also give priority through the API, read the examples/readme.
 Config.Priority = {
-    ["steam:110000137385a24"] = 1,
+    ["steam:11000010f7659e3"] = 1,
     ["steam:1100001465426f1"] = 25,
     ["steam:110000117739ce6"] = 85
 }
 
--- require people to run steam
 Config.RequireSteam = true
 
--- "whitelist" only server
 Config.PriorityOnly = false
 
--- disables hardcap, should keep this true
 Config.DisableHardCap = true
 
--- will remove players from connecting if they don't load within: __ seconds; May need to increase this if you have a lot of downloads.
--- i have yet to find an easy way to determine whether they are still connecting and downloading content or are hanging in the loadscreen.
--- This may cause session provider errors if it is too low because the removed player may still be connecting, and will let the next person through...
--- even if the server is full. 10 minutes should be enough
 Config.ConnectTimeOut = 600
 
--- will remove players from queue if the server doesn't recieve a message from them within: __ seconds
 Config.QueueTimeOut = 90
 
--- will give players temporary priority when they disconnect and when they start loading in
 Config.EnableGrace = false
 
--- how much priority power grace time will give
 Config.GracePower = 5
 
--- how long grace time lasts in seconds
 Config.GraceTime = 480
 
--- on resource start, players can join the queue but will not let them join for __ milliseconds
--- this will let the queue settle and lets other resources finish initializing
 Config.JoinDelay = 30000
 
--- will show how many people have temporary priority in the connection message
 Config.ShowTemp = false
 
--- simple localization
 Config.Language = {
-    joining = "\xF0\x9F\x8E\x89Tilslutter...",
-    connecting = "\xE2\x8F\xB3Tilslutter...",
-    idrr = "\xE2\x9D\x97[KØ] Fejl: Kunne ikke hente nogle af dine id'er, prøv at genstarte.",
-    err = "\xE2\x9D\x97[KØ] Der er en fejl",
-    pos = "\xF0\x9F\x90\x8CDu er nummer %d/%d i køen \xF0\x9F\x95\x9C%s",
-    connectingerr = "\xE2\x9D\x97[KØ] Fejl: Fejl ved at tilføje dig til forbindelsesliste",
-    timedout = "\xE2\x9D\x97[KØ] Fejl: Timed out",
-    wlonly = "\xE2\x9D\x97[KØ] Du skal være whitelisted for at joine serveren Discord.gg/zFRgZzG",
-    steam = "\xE2\x9D\x97 [KØ] Fejl: Steam skal køre"
+    joining = "\xE2\x98\x81Forbinder...",
+    connecting = "\xE2\x98\x81Forbinder...",
+    idrr = "\xE2\x98\x81[KØ] Fejl: Kunne ikke hente nogle af dine id'er, prøv at genstarte.",
+    err = "\xE2\x98\x81[KØ] Der er en fejl",
+    pos = "\xE2\x98\x81Du er nummer %d/%d i køen \xF0\x9F\x95\x9C%s",
+    connectingerr = "\xE2\x98\x81[KØ] Fejl: Fejl ved at tilføje dig til forbindelsesliste",
+    timedout = "\xE2\x98\x81[KØ] Fejl: Timed out",
+    wlonly = "\xE2\x98\x81[KØ] Du skal være whitelisted for at joine serveren Discord.gg/zFRgZzG",
+    steam = "\xE2\x98\x81 [KØ] Fejl: Steam skal køre"
 }
