8fe9b30cf67fb0c7f38a3fae38e4bb58375ccd5f
Mikkel <44027546+FlaxDoe@users.noreply.github.com>
2023-06-04 15:25:22 +0200

    Update jns_server.lua

--- resources/[misc]/watermark/jns_server.lua
+++ resources/[misc]/watermark/jns_server.lua
@@ -1,17 +1,4 @@
-HT = {}
-
 TriggerEvent('HT_base:getBaseObjects', function(obj) HT = obj end)
-
-
-HT.RegisterServerCallback('getPlayers', function(playerCount, cb)
-    local playerCount = GetNumPlayerIndices()
-    cb(playerCount)
-end)
-
-HT.RegisterServerCallback('getPlayers2', function(playerCount, cb)
-    local playerCount = GetNumPlayerIndices()
-    cb(playerCount)
-end)
 
 RegisterNetEvent('getPlayers:check')
 AddEventHandler('getPlayers:check', function()
